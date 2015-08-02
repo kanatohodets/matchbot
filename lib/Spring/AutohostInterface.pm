53f31a8bb80d640aa81fa8ab0a21aa8a7c764e8b
Ben Tyler <benjamin.tyler@gmail.com>
2015-08-02 21:27:53 +0200

    Basic UDP send/recv for AutohostInterface

--- lib/Spring/AutohostInterface.pm
+++ lib/Spring/AutohostInterface.pm
@@ -1,1 +1,51 @@
 package Spring::AutohostInterface;
+use Mojo::Base -strict;
+use Mojo::IOLoop;
+use IO::Socket::IP;
+
+sub new {
+	my ($self) = shift->SUPER::new(@_);
+	my $spring_port = shift;
+	$self->{sock} = IO::Socket::IP->new(
+		PeerAddr => '127.0.0.1',
+		PeerPort => $spring_port,
+		Proto => 'udp',
+		Blocking => 0
+	);
+
+	if (!$self->{sock}) {
+		die "could not bind autohost interface port: $!";
+	}
+
+	$self->{send_buffer} = [];
+
+	Mojo::IOLoop->singleton->reactor->io($sock => sub {
+		my ($readable, $writable) = @_;
+		if ($readable) {
+			# read up to one byte more than max UDP packet size for IPv4
+			$sock->recv(my $buffer, 65_508, 0);
+			if ($buffer) {
+				$self->parse_message($buffer);
+			}
+		}
+
+		if ($writable && @{$self->{send_buffer}}) {
+			my $message = pop @{$self->{send_buffer}};
+			$sock->send($message, 0);
+		}
+	});
+
+	return $self;
+}
+
+sub send {
+	my ($self, $message) = @_;
+	unshift @{$self->{send_buffer}}, $message;
+}
+
+sub parse_message {
+	my ($self, $message) = @_;
+	say "got a message from spring on autohost interface: $message";
+}
+
+1;
