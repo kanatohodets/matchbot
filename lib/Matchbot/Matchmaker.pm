8d544bbd0055cae78e5762b31c6c00546e4b804c
Ben Tyler <benjamin.tyler@gmail.com>
2015-08-08 10:19:44 +0200

    Pick randomly from game and map pools for matches.

--- lib/Matchbot/Matchmaker.pm
+++ lib/Matchbot/Matchmaker.pm
@@ -1,6 +1,7 @@
 package Matchbot::Matchmaker;
 use Mojo::Base -strict;
 use Exporter 'import';
+use List::Util qw(shuffle);
 use Data::Dumper qw(Dumper);
 use 5.18.2;
 
@@ -8,18 +9,25 @@ our @EXPORT_OK = qw(find_queue_matches);
 
 sub find_queue_matches {
 	my $queue = shift;
-	my @games;
+	my @matches;
 	#TODO: smart matching logic, extensibility so its easy to write new
 	#matching algos, whatever.
 	my @players = keys %{$queue->{players}};
+	my @game_pool = @{$queue->{gameNames}};
+	my @map_pool = @{$queue->{mapNames}};
 	#just 1v1 for now
 	while (@players) {
 		my ($one, $two) = (shift @players, shift @players);
 		if (defined $one && defined $two) {
-			push @games, {
+			my $map = (shuffle @map_pool)[0];
+			my $game = (shuffle @game_pool)[0];
+			push @matches, {
+				status => 'readycheck',
+				map => $map,
+				game => $game,
 				players => [$one, $two]
 			};
 		}
 	}
-	return @games;
+	return @matches;
 }
